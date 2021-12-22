853790a0c4a74c7465f5417a7e6ec5c011157f92
hkobulov <kobulov.2000@mail.ru>
2021-12-22 21:49:35 +0500

    complete

---
+++ js/main.js
@@ -0,0 +1,43 @@
+let elForm = document.querySelector('.form');
+let elFilmWrapper = document.querySelector('.film-list')
+let filmAlert = document.querySelector('.film-alert');
+let filmAlertText = document.querySelector('.alert-text');
+let elFilmTemplate = document.querySelector('.film-item-temp').content;
+let elFilmInput = document.querySelector('.film-input');
+
+function renderFilms(array, wrapper){
+    
+    if(array){
+        let filmItem = document.createDocumentFragment();
+        
+        array.forEach(item => {
+            let filmClone = elFilmTemplate.cloneNode(true)
+            console.log(item);
+            filmClone.querySelector('.card-image').src = item.Poster;
+            filmClone.querySelector('.card-title').textContent = item.Title;
+            filmClone.querySelector('.card-year').textContent = item.Year;
+            
+            filmItem.appendChild(filmClone);
+        })
+
+        wrapper.innerHTML = null;
+        wrapper.appendChild(filmItem)
+    } else {
+        wrapper.innerHTML = null
+        filmAlert.classList.remove('d-none')
+    }
+}
+
+
+
+elForm.addEventListener('submit', evt => {
+    evt.preventDefault()
+    
+    let inputValue = elFilmInput.value.trim()
+
+    filmAlertText.textContent = inputValue
+    
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=452845b8&s=${inputValue}`)
+    .then(response => response.json())
+    .then(json => renderFilms(json.Search, elFilmWrapper))
+})
